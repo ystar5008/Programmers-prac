6081ca747eea6e226bbd94a6ad7c69676d642ab7
index.ys <96641210+ystar5008@users.noreply.github.com>
2023-04-11 00:34:17 +0900

    [level 1] Title: 내적, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,9 +1,5 @@
 function solution(a, b) {
-    let answer = 0;
 
-    for (let i = 0; i < a.length; i++) {
-        answer += a[i] * b[i];
-    }
 
-    return answer;
+    return a.map((e, i) => e * b[i]).reduce((a, b) => a + b, 0)
 }
