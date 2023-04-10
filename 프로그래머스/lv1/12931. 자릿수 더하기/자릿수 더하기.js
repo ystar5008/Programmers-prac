ad9727c440dfeace361ff04c8ab80f09e5980e9e
index.ys <96641210+ystar5008@users.noreply.github.com>
2023-04-10 21:21:30 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,11 +1,7 @@
 function solution(n) {
-  let answer = 0;
-  // n을 문자열로 변환
-  let strn = n.toString();
-
-  // for반복문을 통해서 각 자리마다 순회하며 sum 하기
-  for (let i = 0; i < strn.length; i++) {
-    answer += Number(strn[i]);
-  }
+  let answer = String(n) // ["1", "2", "3"]
+    .split("")
+    .map((e) => Number(e))
+    .reduce((a, b) => a + b, 0); // 0+"1" => "01"
   return answer;
 }
