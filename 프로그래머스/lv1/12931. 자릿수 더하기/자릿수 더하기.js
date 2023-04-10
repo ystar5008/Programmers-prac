d3678d8b034d8d786f4f54748a6e69822d33e42f
index.ys <96641210+ystar5008@users.noreply.github.com>
2023-04-10 21:26:02 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,7 +1,14 @@
 function solution(n) {
-  let answer = String(n) // ["1", "2", "3"]
-    .split("")
-    .map((e) => Number(e))
-    .reduce((a, b) => a + b, 0); // 0+"1" => "01"
-  return answer;
+  // let answer = String(n) // ["1", "2", "3"]
+  //   .split("")
+  //   .map((e) => Number(e))
+  //   .reduce((a, b) => a + b, 0); // 0+"1" => "01"
+  // return answer;
+  let answer = 0;
+  for (let i = 0; i < String(n).length; i++) {
+    //"1" "2" "3"
+    answer += Number(String(n)[i]);
+    
+  }
+  return answer
 }
