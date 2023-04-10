157f780f6dc66483eb3e0eedfe9d00e96ff7953b
index.ys <96641210+ystar5008@users.noreply.github.com>
2023-04-10 14:53:41 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,12 @@
+function solution(n) {
+  let answer = "";
+  for (let i = 0; i < n; i++) {
+    if (i % 2 !== 0) {
+      answer += "박";
+    } else {
+      answer += "수";
+    }
+  }
+
+  return answer;
+}
